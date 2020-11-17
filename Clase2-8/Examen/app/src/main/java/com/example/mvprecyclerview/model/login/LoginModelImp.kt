f4e3794980c61e84f0dc04fa72dc171e36359d6b
Charlye Arteaga <charlye934@Charlyes-MacBook-Pro.local>
2020-11-16 18:53:48 -0600

    changes

--- Clase2-8/Examen/app/src/main/java/com/example/mvprecyclerview/model/login/LoginModelImp.kt
+++ Clase2-8/Examen/app/src/main/java/com/example/mvprecyclerview/model/login/LoginModelImp.kt
@@ -2,7 +2,7 @@ package com.example.mvprecyclerview.model.login
 
 import com.example.mvprecyclerview.model.LoginModel
 
-class LoginModelImp: LoginModel {
+class CarlLoginModelImp: LoginModel {
     val nombre = arrayOf("Carlos","Alberto","Daniel","Jose","Daniela","Andrea","Metztli","Samanta","Jaqueline","Paola")
     val telefono = arrayOf("123456","125342","1234643","24120","4363125","123456","125342","1234643","24120","4363125")
     val email = arrayOf("charlye934@gmail.com","alberto@gmail.com","daniel@gmail.com","jose@gmail.com","daniela@gmail.com","andrea@gmail.com","metztli@gmail.com","samanta@gmail.com","jaqueline@gmail.com","paola@gmail.com")
